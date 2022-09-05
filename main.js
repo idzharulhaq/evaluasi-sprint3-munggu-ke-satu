01d91021f61b6068c6a073c45764d692f9fa37cf
idzharulhaq <ihaq1013@gmail.com>
2022-09-05 10:42:00 +0700

    evaluasi sprint 3 minggu ke 1

---
+++ main.js
@@ -0,0 +1,18 @@
+// soal 1
+class animal {
+    constructor(name, age) {
+        this.name = 'kucing'
+        this.age = '4 bulan'
+        ismamal = 'boolean'
+    }
+}
+// soal 2
+class rabbit {
+    constructor () {}
+    method_1 () {`eat ${this.name} sedang makan!`}
+}
+// soal 3
+class eangle {
+    constructor () {}
+    method_1 () {`fly ${this.name} sedang terbang`}
+}
